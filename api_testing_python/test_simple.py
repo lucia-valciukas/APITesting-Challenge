01e8efc99ba25634f4c1ff438907526bc2bd4085
luvalciukas <lvalciukas2003@gmail.com>
2025-07-26 21:55:14 -0300

    this is better

--- api_testing_python/test_simple.py
+++ api_testing_python/test_simple.py
@@ -3,10 +3,16 @@ import json
 import requests
 
 
-def first_try():
-    url = "https://pokeapi.co/api/v2/berry/Cheri Berry"
+def test_first_try():
+    url = "https://pokeapi.co/api/v2/berry/3/"
     response = requests.get(url)
-    response.json()
+    data = response.json()
+    assert data["firmness"] == {'name': 'very-soft', 'url': 'https://pokeapi.co/api/v2/berry-firmness/1/'}, "It Does Match"
+  
+def guardar_en_file(response):
+      output_filename = "output_data.json"
+      with open(output_filename, 'w') as json_file:
+        json.dump(response, json_file, indent=4)
 
 
 
